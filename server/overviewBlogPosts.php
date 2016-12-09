386f7457203530604967ef12957df9ca7cd5beb9
denisoetken <denisoetken@hotmail.de>
2016-12-09 08:51:12 +0100

    Jaworski Import
    SQL eingelesen

--- server/overviewBlogPosts.php
+++ server/overviewBlogPosts.php
@@ -1,3 +1,13 @@
 <?php
 
 
+$blogposts = [];
+$pdo = new PDO(DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+
+$statement = $pdo->prepare("SELECT content, timestamp, name FROM posts INNER JOIN users ON posts.user_id=users.id;");
+$statement->execute();
+
+
+$blogposts = $statement->fetchAll();
+
+
