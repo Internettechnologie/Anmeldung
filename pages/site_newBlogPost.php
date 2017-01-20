de05acf817c0c5b10d04d0714092098a4b8a52b5
denisoetken <denisoetken@hotmail.de>
2017-01-20 08:21:25 +0100

    Bootstrapeinbindung

--- pages/site_newBlogPost.php
+++ pages/site_newBlogPost.php
@@ -1,12 +1,32 @@
-
 <article>
 
     <h2>Neuer Blogeintrag</h2>
 
-    <form action="?" method="post">
+    <form action="?" method="post" id="blogpost">
         <textarea rows="15" cols="80" name="blogpost"></textarea>
         <br><br>
         <button>Speichern</button>
+
     </form>
 
 </article>
+
+<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+<script>
+    var configuration =
+    {selector: 'textarea'};
+    tinymce.init(configuration);
+
+    var form = document.querySelector('#blogpost');
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        var text = tinyMCE.activeEditor.getContent();
+        var textarea = document.querySelector('textarea');
+
+        textarea.value = text;
+        if (text != '') {
+            this.submit();
+        }
+    });
+</script>
